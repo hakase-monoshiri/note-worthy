83e7e20fb5cd07ce2bcba3ea455a519ae17fed3f
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-03-30 12:37:54 -0400

    adding fullname to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-    has_secure_password
-    has_many :notes
+  has_secure_password
+  has_many :notes
 
+  def fullname
+    self.first_name + " " + self.last_name
   end
+  
+
+
+end
